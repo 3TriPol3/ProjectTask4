40cf1f2ba10cf02598da4b4b77bd0f1bacbd6da6
SystemAdminTkinter <gancster007@gmail.com>
2026-02-06 14:45:47 +0500

    ...

--- Controllers/TransactionController.py
+++ Controllers/TransactionController.py
@@ -36,6 +36,33 @@ class TransactionController:
     def delete(cls, id):
         Transaction.delete_by_id(id)
 
+    # Найти(фильтровать) транзакцию по категории - category
+    @classmethod
+    def get_category(cls, category):
+        return Transaction.select().where(Transaction.category == category)
+
+    # # Показать баланс
+    # @classmethod
+    # def get_balance(cls, category):
+
+
 if __name__ == "__main__":
+    # TransactionController.add(  # Добавить транзакцию в таблицу
+    #     category="Зарплата",
+    #     amount=60000.00,
+    #     type="Доход",
+    #     date="2024-04-05",
+    #     description="Зарплата за март"
+    # )
+
+    # TransactionController.update(1, category='Авто') # Редактирование транзакции
+
+    for item in TransactionController.get():  # Выводит список записей из таблицы БД
+        print(item.category, item.amount, item.type, item.date, item.description)
+
+    # TransactionController.delete(4) # Удалить транзакцию по - id
+
+    for item in TransactionController.get_category('Продукты'):  # Фильтрация по категории
+        print(item.id, item.category, item.amount, item.type, item.date, item.description)
 
 
--- Models/Transaction.py
+++ Models/Transaction.py
@@ -5,7 +5,7 @@ class Transaction(BaseModel):
     Данный класс описывает таблицу в БД с финансами
     '''
     id = PrimaryKeyField() # id
-    category = CharField(unique=True) # Категория дохода/расхода
+    category = CharField() # Категория дохода/расхода
     amount = FloatField() # Количество (денег)
     type = CharField() # Тип дохода/расхода
     date = DateField() # Дата дохода/расхода
